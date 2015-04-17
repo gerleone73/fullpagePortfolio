6edd14724fdeab44b8bfa1449c44c1a431c5b371
Gerry Leonard <leonardger@gmail.com>
2015-04-17 15:43:21 +0100

    menu overlay track links

--- js/main.js
+++ js/main.js
@@ -27,69 +27,114 @@
            $(".headnav").css('background-color','#F2F2F2');
            $('.Gl').css('visibility','hidden');
            $('.Gl').fadeOut();
+           $('#home').addClass('nav-live');
 
         }
-
-      
 
         if(anchorLink!=='firstPage'){
            $('.Gl').css('visibility','visible');
           $('.Gl').fadeIn();
           $('.menuTip').fadeIn();
+          $('#home').removeClass('nav-live');
+
         }
         if (anchorLink === "secondPage") {
           $(".counter").text("2");
            $(".headnav").css('background-color','#fff');
-         
-         
-       
-        
+         $('#wise').addClass('nav-live');
         }
+        if(anchorLink!=='secondPage'){
+          $('#wise').removeClass('nav-live');
+        }
+
+
         if (anchorLink === "thirdPage") {
           $(".counter").text("3");
 
           $(".headnav").css('background-color','#ebebeb');
-        
-
-     
+        $('#sine').addClass('nav-live');
         }
+         if(anchorLink!=='thirdPage'){
+          $('#sine').removeClass('nav-live');
+        }
+
+
           if (anchorLink === "fourthPage") {
           $(".counter").text("4");
           $(".headnav").css('background-color','#fff');
         
+         $('#styles').addClass('nav-live');
         }
+         if(anchorLink!=='fourthPage'){
+          $('#styles').removeClass('nav-live');
+        }
+
+
           if (anchorLink === "fifthPage") { 
           $(".counter").text("5");
        
          $(".headnav").css('background-color','#ebebeb');
+         $('#kraftwerk').addClass('nav-live');
         }
+         if(anchorLink!=='fifthPage'){
+          $('#kraftwerk').removeClass('nav-live');
+        }
+
+
         if (anchorLink === "sixthPage") {
           $(".counter").text("6");
         
          $(".headnav").css('background-color','#fff');
+        $('#his').addClass('nav-live');
+        }
+         if(anchorLink!=='sixthPage'){
+          $('#his').removeClass('nav-live');
         }
       
         if (anchorLink === "seventhPage") {
           $(".counter").text("7");
         
          $(".headnav").css('background-color','#ebebeb');
+         $('#misc').addClass('nav-live');
         }
+         if(anchorLink!== "seventhPage"){
+          $('#misc').removeClass('nav-live');
+        }
+
+
         if (anchorLink === "eighthPage") {
           $(".counter").text("8");
         
          $(".headnav").css('background-color','#fff');
+         $('#cncf').addClass('nav-live');
         }
+         if(anchorLink!=='eighthPage'){
+          $('#cncf').removeClass('nav-live');
+        }
+
         if (anchorLink === "ninthPage") {
           $(".counter").text("9");
       
          $(".headnav").css('background-color','#ebebeb');
+         $('#crowd').addClass('nav-live');
         }
+         if(anchorLink!=='ninthPage'){
+          $('#crowd').removeClass('nav-live');
+        }
+
+
         if (anchorLink === "tenthPage"){
           $(".counter").text("10");
            $(".headnav").css('background-color','#fff');
           $(".magic").addClass("animated bounceInLeft");
           $(".trophy").addClass("animated bounceInUp");
+          $('#animation').addClass('nav-live');
+          $('#fullBg').removeClass('nav-live');
           return $(".users").addClass("animated bounceInRight");
+         
+        }
+         if(anchorLink!=='tenthPage'){
+          $('#animation').removeClass('nav-live');
         }
 
         
@@ -97,8 +142,13 @@
         if (anchorLink === "eleventhPage") {
           $(".counter").text("11");
            $(".headnav").css('background-color','#ebebeb');
-        
+         $('#fullBg').addClass('nav-live');
         }
+         if(anchorLink!=='eleventhPage'){
+          $('#fullBg').removeClass('nav-live');
+        }
+        
+
         if (anchorLink === "twelfthPage") {
           $(".counter").text("12");
            $(".headnav").css('background-color','#fff');
@@ -116,14 +166,15 @@
                       });    
           
           $('.author').fadeIn(8000);
-         
-         
+          $('#nothing').addClass('nav-live');
         }
+         
           if (anchorLink !== "twelfthPage"){
              $('.nothing').fadeOut(100);
              $('.godard').fadeOut(100);
           $('.author').fadeOut(100);
           $('.birdman').fadeOut(100);
+          $('#nothing').removeClass('nav-live');
           }
       }
     });
