c8ecbe3019699bfc08b52ca0cb66e4dd094813bf
Gerry Leonard <leonardger@gmail.com>
2015-04-23 01:33:10 +0100

    load music only when needed attempt

--- js/main.js
+++ js/main.js
@@ -92,28 +92,7 @@ if (document.documentElement.clientWidth < 1000) {
 
 
      var obj2 = document.createElement("audio");
-        obj2.src="leb wohl edit.mp3";
-        obj2.volume=0.3;
-        obj2.autoPlay=false;
-        obj2.preLoad=true;       
- 
-        
-            
-     $('.stop').on( "click", function() {
-      obj2.load();
-     }); 
-
-     $('.play').on( "click", function() {
-       obj2.play();
-     }); 
-
-     $('.fa-music').on("click",function() {
-      if ( $('#neu').css('visibility') == 'hidden' )
-    $('#neu').css('visibility','visible');
-  else
-    $('#neu').css('visibility','hidden');
-
-     }); 
+      
 
 
 
@@ -270,6 +249,29 @@ if (document.documentElement.clientWidth < 1000) {
 
          if (anchorLink === "twelfthPage"){
           $(".counter").text("12");
+
+            obj2.src="leb wohl edit.mp3";
+        obj2.volume=0.3;
+        obj2.autoPlay=false;
+        obj2.preLoad=true;       
+ 
+        
+            
+     $('.stop').on( "click", function() {
+      obj2.load();
+     }); 
+
+     $('.play').on( "click", function() {
+       obj2.play();
+     }); 
+
+     $('.fa-music').on("click",function() {
+      if ( $('#neu').css('visibility') == 'hidden' )
+    $('#neu').css('visibility','visible');
+  else
+    $('#neu').css('visibility','hidden');
+
+     }); 
           obj2.load();
             $('.nothing').fadeOut(100);
              $('.godard').fadeOut(100);
